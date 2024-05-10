a45d0ceded6b2a92c6c0ba22cd847f2b5195093c
Juandavid716 <jbojato@uninorte.edu.co>
2024-05-10 13:06:42 -0500

    chore: add api handler for nextauth

---
+++ apps/next/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,38 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from 'app/auth/firebase';
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  pages: {
+    signIn: '/sign-in',
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {},
+      async authorize(credentials): Promise<any> {
+        return await signInWithEmailAndPassword(
+          auth,
+          (credentials as any).email || '',
+          (credentials as any).password || '',
+        )
+          .then(userCredential => {
+            if (userCredential.user) {
+              return userCredential.user;
+            }
+            return null;
+          })
+          .catch(error => console.log(error))
+          .catch(error => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(error);
+          });
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
+export default NextAuth(authOptions);
